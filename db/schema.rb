bc05ba632f2fff6ef3f77ae628b28f904f23841c
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-15 05:36:31 +0900

    seedファイルでユーザーを作成動作確認

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_12_091143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "description", null: false
-    t.integer "status", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
