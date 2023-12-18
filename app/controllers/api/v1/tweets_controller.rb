f58e050fbfa35333b92471b01819c9305631a30d
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-18 12:31:08 +0900

    createアクション作成

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -11,4 +11,18 @@ class Api::V1::TweetsController < ApplicationController
     render json: { tweet: tweet }
   end
 
+  def create
+    post = current_user.tweets.build(post_params)
+    if post.save
+      render json: { post: post }, status: :ok
+    else
+      render json: { error_message: post.errors.full_messages}, status: :bad
+    end
+  end
+
+  private
+  def post_params
+    params.require(:tweet).permit(:tweet_content, :image)
+  end
+
 end
