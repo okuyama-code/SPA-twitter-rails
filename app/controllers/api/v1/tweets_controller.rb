84085bc337b7baac5d5864cb764be7e7959a6d1c
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-19 05:13:19 +0900

    current_userにフロントからデータを送り。使えるようにした。

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -11,10 +11,26 @@ class Api::V1::TweetsController < ApplicationController
     render json: { tweet: tweet }
   end
 
+  # Reactからcurrent_user.idを送るパターン
+  # def create
+  #   pp "デバック！！！！！！！！！！！！！"
+  #   pp params
+  #   tweet = Tweet.new(tweet_params)
+
+  #   if tweet.save!
+  #     render json: { tweet: tweet }, status: :ok
+  #   else
+  #     render json: { error_message: tweet.errors.full_messages }, status: :bad
+  #   end
+  # end
+
+  # current_userを使うほう
   def create
     pp "デバック！！！！！！！！！！！！！"
-    pp params
-    tweet = Tweet.new(tweet_params)
+    # pp params
+    pp current_user
+
+    tweet = current_user.tweets.build(tweet_params)
 
     if tweet.save!
       render json: { tweet: tweet }, status: :ok
