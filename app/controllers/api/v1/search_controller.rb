de1c790169c1b12f18e7cac7f6312d71daa5bd34
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-01-01 07:07:28 +0900

    pagination動作確認

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,17 +2,16 @@ class Api::V1::SearchController < ApplicationController
    # http://localhost:3000/api/v1/search/
   # http://localhost:3000/api/v1/search/?q=a
   def index
+    posts_per_page = 4
     @posts = Post.where('post_content LIKE ?', "%#{params[:q]}%").order(created_at: :desc)
 
+    posts_with_images = paginate_posts(@posts, posts_per_page)
+    total_posts_count = @posts.count
 
-    posts_with_images = @posts.map do |post|
-      if post.image.attached?
-        post.as_json.merge(image_url: url_for(post.image))
-      else
-        post.as_json.merge(image_url: nil)
-      end
-    end
-
-    render json: posts_with_images
+    render json: {
+      posts: posts_with_images,
+      total_count: total_posts_count,
+      per_page: posts_per_page
+    }
   end
 end
