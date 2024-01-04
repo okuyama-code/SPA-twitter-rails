13a35c7ac1e72769cd1c789d409e372a2f9ca616
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-01-04 18:57:47 +0900

    途中まで

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,7 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
   def create
+    pp "デバック！！！！！！！！！！！！！"
+    pp current_user
+    @comment = Comment.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      render json: {comment: @comment}
+    else
+      render json : { message: "コメントの作成に失敗しました。"}
+    end
   end
 
+  # 修正必要かも
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment_content, :post_id)
+    # params.require(:comment).permit(:comment_content, :comment_img)
+    # formにてpost_idパラメータを送信して、コメントへpost_idを格納するようにする必要がある。
   end
 end
