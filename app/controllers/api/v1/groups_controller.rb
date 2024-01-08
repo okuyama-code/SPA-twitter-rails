01d7a5d7ea0cb135934dac10136ad69db25faca3
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-01-08 18:20:51 +0900

    修正

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -5,39 +5,33 @@ module Api
     class GroupsController < ApplicationController
       # http://localhost:3000/api/v1/groups
       def index
-        # ログインユーザー所属ルームID取得
-        # current_entries = current_user.entries
-        current_entries = User.find(1).entries
+        current_entries = current_user.entries
         my_group_id = []
         current_entries.each do |entry|
           # エントリーから取得したルームIDを my_room_id 配列に追加しています。
           my_group_id << entry.group.id
         end
         # 自分のroom_idと同じでuser_idが自分ではないentryを取得
-        # another_entries = Entry.where(group_id: my_group_id).where.not(user_id: current_user.id)
-        another_entries = Entry.where(group_id: my_group_id).where.not(user_id: 1)
+        another_entries = Entry.where(group_id: my_group_id).where.not(user_id: current_user.id)
+
 
         render json: {current_entries: current_entries, another_entries: another_entries}
       end
 
       def show
-        @room = Room.find(params[:id])
-        @messages = @room.messages.all
-        @message = Message.new
-        @entries = @room.entries
-        @another_entry = @entries.where.not(user_id: current_user.id).first
-      end
-
-      # before_action :authenticate_user!
-      # users/showページで@is_roomがない時にformでパラメーターが送られてきてcreateアクションが走る。
-      # 現在ログインしているユーザーとメッセージ相手のユーザーそれぞれの情報をroom_idで紐付けてEntryテーブルに２つレコードを作成している。
-
-      # TODO RoomをGroupに変換
-      # params[:user_id]でshowのidを渡す
-      def create
         pp "デバック！！！！！！！！！！！！！"
         pp current_user
-        pp params[:user_id]
+
+        @group = Group.find(params[:id])
+        @messages = @group.messages.all
+        @message = Message.new
+        @entries = @group.entries
+        @another_entry = @entries.where.not(user_id: current_user.id).first
+
+        render json: {messages: @messages, another_entry: @another_entry }
+      end
+
+      def create
         ActiveRecord::Base.transaction do
           group = Group.create(user_id: current_user.id)
           Entry.create!(user_id: current_user.id, group_id: group.id)
