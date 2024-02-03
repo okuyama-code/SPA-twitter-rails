7f20dc548b954ac92fe8bad7aa76ebd5624251e6
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-02-03 17:19:07 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follower, dependent: :destroy
 
 
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
-  has_many :followers, through: :reverse_of_relationships, source: :following
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :following, dependent: :destroy
 
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
